5448e7edd0e8f084efc808e18191757a6d2ac93c
AfonsoMota-132 <afonsomota88@gmail.com>
2024-11-05 21:15:44 +0000

    res

--- norminette_const.sh
+++ norminette_const.sh
@@ -4,15 +4,37 @@ BLUE='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 BWhite='\033[1;37m' 
-while true; do
-	clear
-	OK_NBR=$(norminette | grep -c "OK")
-	KO_NBR=$(norminette | grep -c "Error!")
-	let "TOTAL=$OK_NBR+$KO_NBR"
-	let "PERCENTAGE=$OK_NBR*100/$TOTAL"
-	echo "Ok		:" $OK_NBR "/" $TOTAL " "
-	echo "Error		:" $KO_NBR "/" $TOTAL " "
-#	echo "Total		:" $TOTAL " "
-	echo "Percentage	:" $PERCENTAGE "% "
-	sleep 0.5
-done
+function ProgressBar {
+# Process data
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+echo -e "${BLUE}Progress : [${GREEN}${_fill// /#}${RED}${_empty// /-}${BLUE}] ${_progress}%"
+
+}
+if (norminette | grep -c ":") > 0; then
+	while true; do
+		clear
+		OK_NBR=$(norminette | grep -c "OK")
+		KO_NBR=$(norminette | grep -c "Error!")
+		STR=$(norminette | grep "Error")
+		let "TOTAL=$OK_NBR+$KO_NBR"
+		if [ $KO_NBR -eq 0 ]; then
+			echo -e "${GREEN}Everything's according to norm!"
+			break
+		fi
+		let "PERCENTAGE=$OK_NBR*100/$TOTAL"
+		ProgressBar ${OK_NBR} ${TOTAL}	
+		echo -e "${BLUE}Ok		:${GREEN}" $OK_NBR "/" $TOTAL " "
+		echo -e "${BLUE}Error		:${RED}" $KO_NBR "/" $TOTAL " "
+		echo -e "${BLUE}Percentage	:" $PERCENTAGE "% "
+		echo "$STR"
+		sleep 1
+		clear
+	done
+else
+	echo -e "${RED}No files found for Norminette"
+fi
