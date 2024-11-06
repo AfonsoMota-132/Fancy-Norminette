d2e40bd0adebdbaddc81f4e59bc7f44175dacb3c
Afonso Goncalves Mota <afogonca@c1r15s5.42porto.com>
2024-11-06 10:58:09 +0000

    yippiee

--- norminette_const.sh
+++ norminette_const.sh
@@ -3,6 +3,7 @@ TE='\033[0;37m'
 BLUE='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 BWhite='\033[1;37m' 
 function ProgressBar {
 # Process data
@@ -23,18 +24,34 @@ if (norminette | grep -c ":") > 0; then
 		STR=$(norminette | grep "Error")
 		let "TOTAL=$OK_NBR+$KO_NBR"
 		if [ $KO_NBR -eq 0 ]; then
+			clear
+			echo -e "${BLUE}╔══════════════════════════════════════════╗ "
+			echo -e "${BLUE}║                                          ║ "
+			echo -e "${BLUE}║   ${GREEN}🎉 Everything's according to norm 🎉 ${BLUE}  ║ "
+			echo -e "${BLUE}║                                          ║ "
+			echo -e "${BLUE}╚══════════════════════════════════════════╝ "
 			echo -e "${GREEN}Everything's according to norm!"
 			break
 		fi
 		let "PERCENTAGE=$OK_NBR*100/$TOTAL"
+		echo -e "${BLUE}╔══════════════════════════════════════════╗ "
+		echo -e "${BLUE}║                                          ║ "
+		echo -e "${BLUE}║    Welcome to ✨ ${YELLOW}Improved ✨ ${BLUE}Norminette  ║ "
+		echo -e "${BLUE}║                                          ║ "
+		echo -e "${BLUE}╚══════════════════════════════════════════╝ "
 		ProgressBar ${OK_NBR} ${TOTAL}	
 		echo -e "${BLUE}Ok		:${GREEN}" $OK_NBR "/" $TOTAL " "
 		echo -e "${BLUE}Error		:${RED}" $KO_NBR "/" $TOTAL " "
 		echo -e "${BLUE}Percentage	:" $PERCENTAGE "% "
 		echo "$STR"
-		sleep 1
+		sleep 0.5
 		clear
 	done
 else
-	echo -e "${RED}No files found for Norminette"
+	clear
+	printf "$RED╔═════════════════════════════════════╗ \n"
+	printf "$RED║                                     ║\n"
+	printf "$RED║    No files found for Norminette    ║\n"
+	printf "$RED║                                     ║\n"
+	printf "$RED╚═════════════════════════════════════╝ "
 fi
---
+++ test.sh
@@ -0,0 +1,16 @@
+RBLACK='\033[0;30m'        # Black
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
+BLUE='\033[0;34m'         # Blue
+PURPLE='\033[0;35m'       # Purple
+CYAN='\033[0;36m'         # Cyan
+WHITE='\033[0;37m'        # WhiteED='\033[0;31m'
+
+
+echo '\e]4;14;rgb:ff/00/00\a\e[38;5;14mThis is Red\e[m'
+echo '\e]4;15;rgb:ff/a0/00\a\e[38;5;15mThis is Red\e[m'
+echo '\e]4;16;rgb:ff/d0/00\a\e[38;5;16mThis is Orange\e[m'
+echo '\e]4;17;rgb:ff/ff/00\a\e[38;5;17mThis is yellow\e[m'
+echo '\e]4;18;rgb:a3/ff/00\a\e[38;5;18mThis is light green\e[m'
+echo '\e]4;19;rgb:00/ff/00\a\e[38;5;19mThis is Green\e[m'
