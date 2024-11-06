2c07fa2a32f2ed8fb88a0a7f8be667defc424ee4
AfonsoMota-132 <afonsomota88@gmail.com>
2024-11-06 21:42:02 +0000

    res

--- utils/install_zshrc.sh
+++ utils/install_zshrc.sh
@@ -5,8 +5,13 @@ RC_FILE="$HOME/.zshrc"
 if ! grep "fancynorm=" "$RC_FILE"; then
 	printf "\nalias fancynorm='~/scripts_shell/fancy_norm/run.sh ${1} ${2} | grep %c %c' \n" '"' '"' >> "$RC_FILE"
 fi
-
+clear
 if ! grep "fnorm=" "$RC_FILE"; then
 	printf "\nalias fnorm='~/scripts_shell/fancy_norm/run.sh ${1} ${2} | grep %c %c'\n"  '"' '"' >> "$RC_FILE"
 fi
+clear
+if ! grep "fanorm=" "$RC_FILE"; then
+	printf "\nalias fanorm='~/scripts_shell/fancy_norm/run.sh | grep %c %c'\n"  '"' '"' >> "$RC_FILE"
+fi
+clear
 
