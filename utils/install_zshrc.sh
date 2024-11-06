6890424ae60060028da02eb545c0ee8c9c3bcc7c
Afonso Goncalves Mota <afogonca@c1r15s5.42porto.com>
2024-11-06 13:29:29 +0000

    res

--- utils/install_zshrc.sh
+++ utils/install_zshrc.sh
@@ -1,10 +1,10 @@
 RC_FILE="$HOME/.zshrc"
 
-if ! grep "fancynorm=" "$RC_FILE" &> /dev/null; then
-	printf "\nalias fancynorm=$HOME/scripts_shell/fancy_norm/run.sh\n" >> "$RC_FILE"
+if ! grep "fancynorm=" "$RC_FILE"; then
+	printf "\nalias fancynorm='~/scripts_shell/fancy_norm/run.sh | grep %c %c' \n" '"' '"' >> "$RC_FILE"
 fi
 
-if ! grep "fnorm=" "$RC_FILE" &> /dev/null; then
-	printf "\nalias fnorm=$HOME/scripts_shell/fancy_norm/run.sh\n" >> "$RC_FILE"
+if ! grep "fnorm=" "$RC_FILE"; then
+	printf "\nalias fnorm='~/scripts_shell/fancy_norm/run.sh | grep %c %c'\n" '"' '"' >> "$RC_FILE"
 fi
 
